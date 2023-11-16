7e6b10bd5d4fc74df527e2097a63169c24ce1c01
Breno Marcomini <brenomarcomini@hotmail.com>
2023-11-16 19:10:09 -0300

    data

---
+++ Curso 01/Data/Aula01.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+ a = 'Breno'
+ b = 12
+ c = [1,2,3,4,5,6,7]
+ d = (:Breno)
+ e = {
+  'nome': 'Breno',
+}
+
+puts a.class
+puts b.class
+puts c.class
+puts d.class
+puts e.class
