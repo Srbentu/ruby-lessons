abc50c89cd3c064a08eb8200954e62f801db260e
Breno Marcomini <breno.marcomini@vitat.com.br>
2023-11-17 15:59:47 -0300

    aray

--- Curso 01/Hello world/aula01.rb
+++ Curso 01/Hello world/aula01.rb
@@ -1,3 +1,6 @@
 # frozen_string_literal: true
 
 puts "hello word"
+
+
+https://vitat.com.br/vitat-cuida/checkout?p=clube_6-meses_6x-89_renov-mensal-14_1-consulta_b2c_20230522&utm_source=vitat&utm_medium=owned&utm_campaign=black_friday_vitat_cuida_lp_builder_b&utm_content=landing_page_b
---
+++ Curso 02/Array/Array.rb
@@ -0,0 +1,11 @@
+#lista de livros
+livros = []
+livros = ["Harry Potter", "Senhor dos Aneis", "Percy Jackson"]
+#imprimindo o array
+puts livros[0]
+puts livros[1]
+puts livros[2]
+
+livros.each do |livro|
+    puts livro
+end
