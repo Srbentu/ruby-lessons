82ee60184cde5371b3fe29300372052eeafae6d5
Breno Marcomini <brenomarcomini@hotmail.com>
2023-11-18 15:27:07 -0300

    POlimorfismo

--- Curso 03/Polimorfistmo/Polimorfismo.rb
+++ Curso 03/Polimorfistmo/Polimorfismo.rb
@@ -1,2 +1,23 @@
 # frozen_string_literal: true
 
+class Objeto
+  def  escrever
+    puts "Escrevendo"
+  end
+end
+
+class Lapis < Objeto
+  def escrever
+    puts "Escrevendo a Lapis"
+  end
+end
+
+class Teclado < Objeto
+
+end
+
+pentel = Lapis.new
+pentel.escrever
+
+kiboom = Teclado.new
+kiboom.escrever
