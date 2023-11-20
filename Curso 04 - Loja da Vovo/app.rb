b76926d96e2c9a657ba494997bdce1c1f1f3976d
Breno Marcomini <brenomarcomini@hotmail.com>
2023-11-19 23:01:11 -0300

    regra de negocio

--- Curso 04 - Loja da Vovo/app.rb
+++ Curso 04 - Loja da Vovo/app.rb
@@ -1,2 +1,10 @@
 # frozen_string_literal: true
 
+require_relative './loja'
+require_relative './produto'
+
+produto = Produto.new
+produto.nome = "Forma de Bolo 30 cm"
+produto.preco = 50.00
+
+Loja.new(produto.nome, produto.preco).comprar
--- Curso 04 - Loja da Vovo/loja.rb
+++ Curso 04 - Loja da Vovo/loja.rb
@@ -1,2 +1,12 @@
 # frozen_string_literal: true
 
+class Loja
+  def initialize(produto, preco)
+      @produto = produto
+      @preco = preco
+  end
+
+  def comprar
+    puts "VOcê comprou o produto #{@produto} pelo valor de R$#{@preco}"
+  end
+end
