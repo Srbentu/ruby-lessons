5b3989d4bac7c3fc58bd2b90c489dd36abaed212
Breno Marcomini <breno.marcomini@vitat.com.br>
2023-11-22 09:34:14 -0300

    module

---
+++ Curso 06/module.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module PalavraReversa
+  def self.puts text
+    print text.reverse.to_s
+  end
+end
+
+PalavraReversa::puts "Breno Marcomini"
+puts "Breno Marcomini"
